1fdc0341957ecf8c50d0634a08e4d564fb76ed51
chenjianlei <woshixiaolei@qq.com>
2020-03-16 17:13:10 +0800

    add

--- Sources/LZSwiftPage/LZSwiftPage.swift
+++ Sources/LZSwiftPage/LZSwiftPage.swift
@@ -1,10 +1,10 @@
-struct LZSwiftPage {
+public struct LZSwiftPage {
     var text = "Hello, World!"
-    func v1() {
+    public func v1() {
         print("v1")
     }
     
-    func v2() {
+    public func v2() {
         print("v2")
     }
 }
